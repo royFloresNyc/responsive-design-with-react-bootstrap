22193c56f66868c3fa0ab491faba24a212e11111
royFloresNyc <royfloresnyc@gmail.com>
2021-01-24 20:55:53 -0500

    Create Responsive Navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './Components/Home';
-
+import Panda from './Components/Panda';
+import Gorilla from './Components/Gorilla';
+import Rhino from './Components/Rhino';
+import SeaTurtle from './Components/SeaTurtle';
+import Navigation from './Components/Navigation';
 
 function App() {
     return (
         <div className="App">
-            <Home/>
+            <Navigation/>
+            <Switch>                
+                <Route exact path='/' component={Home}/>            
+                <Route path='/panda' component={Panda}/>  
+                <Route path='/gorilla' component={Gorilla}/>               
+                <Route path='/rhino' component={Rhino}/> 
+                <Route path='/seaTurtle' component={SeaTurtle}/>  
+            </Switch>
         </div>
     );
 }
